b39d925dac73ad93794c431b58d7aed20830cd1b
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-09-29 15:24:24 +0200

    Deleted unused files

--- Exercise 8/Task 1/openssl-example/task1.cpp
+++ Exercise 8/Task 1/openssl-example/task1.cpp
@@ -17,11 +17,17 @@ std::string hex(const std::string &input) {
 /// Return the SHA-512 (512-bit) hash from input.
 std::string sha1(const std::string &input) {
   std::string hash;
-  hash.resize(512 / 8);
-  SHA512((const unsigned char *)&input[0], input.size(), (unsigned char *)&hash[0]);
+  hash.resize(160 / 8);
+  SHA1((const unsigned char *)&input[0], input.size(), (unsigned char *)&hash[0]);
   return hash;
 }
 
 int main() {
-  std::cout << hex(sha1("Password123")) << std::endl;
+  std::string input = "QwE";
+  input = "Saltet til Ola" + input;
+  std::cout << "Task1!" << std::endl;
+  for (int i = 0; i < 2048; i++) {
+    input = hex(sha1(input));
+  }
+  std::cout << input << std::endl;
 }
