3bd76b01d2c3edfce593e93d6d868a3481448802
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-10-20 14:59:11 +0200

    Started Task 3. Changed modulo method in task 2 a)

--- Crypto 1/Task 2.py
+++ Crypto 1/Task 2.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
 
     for x in range(11):
         for y in range(11):
-            z12[x][y] = ((x+1) * (y+1)) % 12
+            z12[x][y] = ((x+1) * (y+1))
+
+    z12 = np.mod(z12, 12)
 
     print(z12)
 
@@ -22,3 +24,7 @@ if __name__ == '__main__':
         for y in range(11):
             if z12[x][y] == 1:
                 print(f'{x+1} has a multiplicative inverse with mod 12')
+
+    print("c)")
+
+    # TODO:
